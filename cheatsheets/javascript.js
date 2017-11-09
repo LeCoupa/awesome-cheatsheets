2b43adf72cc3ca8faa1c5e6d0a3bfc12309158d5
Julien Le Coupanec <julien.le.coupanec@gmail.com>
2017-11-09 02:49:11 +0000

    JavaScript: global objects > array

---
+++ cheatsheets/javascript.js
@@ -0,0 +1,51 @@
+/* *******************************************************************************************
+ * GLOBAL OBJECTS > ARRAY
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+ * ******************************************************************************************* */
+
+
+// Global object: properties
+Array.length                                         // Reflects the number of elements in an array
+
+// Global object: methods
+Array.from(arrayLike[, mapFn[, thisArg]])            // Creates a new Array instance from an array-like or iterable object.
+Array.isArray(obj)                                   // Returns true if a variable is an array, if not false.
+Array.of(element0[, element1[, ...[, elementN]]])    // Creates a new Array instance with a variable number of arguments, regardless of number or type of the arguments.
+
+// Instance: properties
+arr.length                                           // Reflects the number of elements in an array.
+
+// Instance: mutator methods
+arr.copyWithin(target, start, end)                   // Copies a sequence of array elements within the array.
+arr.fill(value, start, end)                          // Fills all the elements of an array from a start index to an end index with a static value.
+arr.pop()                                            // Removes the last element from an array and returns that element.
+arr.push([element1[, ...[, elementN]]])              // Adds one or more elements to the end of an array and returns the new length of the array.
+arr.reverse()                                        // Reverses the order of the elements of an array in place — the first becomes the last, and the last becomes the first.
+arr.shift()                                          // Removes the first element from an array and returns that element.
+arr.sort()                                           // Sorts the elements of an array in place and returns the array.
+array.splice(start, deleteCount, item1, item2, ...)  // Adds and/or removes elements from an array.
+arr.unshift([element1[, ...[, elementN]]])           // Adds one or more elements to the front of an array and returns the new length of the array.
+
+// Instance: accessor methods
+arr.concat(value1[, value2[, ...[, valueN]]])        // Returns a new array comprised of this array joined with other array(s) and/or value(s).
+arr.includes(searchElement, fromIndex)               // Determines whether an array contains a certain element, returning true or false as appropriate.
+arr.indexOf(searchElement[, fromIndex])              // Returns the first (least) index of an element within the array equal to the specified value, or -1 if none is found.
+arr.join(separator)                                  // Joins all elements of an array into a string.
+arr.lastIndexOf(searchElement, fromIndex)            // Returns the last (greatest) index of an element within the array equal to the specified value, or -1 if none is found.
+arr.slice(begin, end)                                // Extracts a section of an array and returns a new array.
+arr.toString()                                       // Returns a string representing the array and its elements. Overrides the Object.prototype.toString() method.
+arr.toLocaleString(locales, options)                 // Returns a localized string representing the array and its elements. Overrides the Object.prototype.toLocaleString() method.
+
+// Instance: iteration methods
+arr.entries()                                        // Returns a new Array Iterator object that contains the key/value pairs for each index in the array.
+arr.every(callback[, thisArg])                       // Returns true if every element in this array satisfies the provided testing function.
+arr.filter(callback[, thisArg])                      // Creates a new array with all of the elements of this array for which the provided filtering function returns true.
+arr.find(callback[, thisArg])                        // Returns the found value in the array, if an element in the array satisfies the provided testing function or undefined if not found.
+arr.findIndex(callback[, thisArg])                   // Returns the found index in the array, if an element in the array satisfies the provided testing function or -1 if not found.
+arr.forEach(callback[, thisArg])                     // Calls a function for each element in the array.
+arr.keys()                                           // Returns a new Array Iterator that contains the keys for each index in the array.
+arr.map(callback[, initialValue])                    // Creates a new array with the results of calling a provided function on every element in this array.
+arr.reduce(callback[, initialValue])                 // Apply a function against an accumulator and each value of the array (from left-to-right) as to reduce it to a single value.
+arr.reduceRight(callback[, initialValue])            // Apply a function against an accumulator and each value of the array (from right-to-left) as to reduce it to a single value.
+arr.some(callback[, initialValue])                   // Returns true if at least one element in this array satisfies the provided testing function.
+arr.values()                                         // Returns a new Array Iterator object that contains the values for each index in the array.
