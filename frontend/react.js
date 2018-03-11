e76a374d90a8390acb1d15a055f519c0335973a8
Julien Le Coupanec <julien.le.coupanec@gmail.com>
2018-03-10 17:08:57 -0800

    Add React.js

---
+++ frontend/react.js
@@ -0,0 +1,8 @@
+/* *******************************************************************************************
+ * GLOBAL CONFIG
+ * Vue.config is an object containing Vue’s global configurations.
+ * You can modify its properties listed below before bootstrapping your application.
+ * https://vuejs.org/v2/api/#Global-Config
+ * ******************************************************************************************* */
+
+
