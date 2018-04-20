44e9641f7867038aeea88a72a06e316ffea2b190
kobus <kobus.haarhuis@hotmail.com>
2018-04-20 09:59:34 +0200

    Die at start of file

--- languages/php.php
+++ languages/php.php
@@ -1,5 +1,9 @@
 <?php
 
+// Exit the file, string inside get's echo'ed
+die("This file is not ment to be ran. ¯\_(ツ)_/¯");
+exit("This file is not ment to be ran. ¯\_(ツ)_/¯");
+
 /**
  * Printing
  */
