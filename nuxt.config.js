a54ae3f32a582696dffd6dc83ab52b9faaa4c00e
Julien <julien.le.coupanec@gmail.com>
2018-09-10 00:25:24 +0200

    build(ga): set ssr to false

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,10 @@ module.exports = {
         : "http://localhost:3000"
   },
   css: ["normalize.css/normalize.css"],
-  plugins: [{ src: "@/plugins/ga.js" }, { src: "@/plugins/global.js" }],
+  plugins: [
+    { src: "@/plugins/ga.js", ssr: false },
+    { src: "@/plugins/global.js" }
+  ],
   build: {
     extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
