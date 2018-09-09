ce7ade78717cca049c3e56cda336657d1b4b08ab
Julien <julien.le.coupanec@gmail.com>
2018-09-09 17:06:11 +0200

    build(packages): add pug and sass loader + normalize.css

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,39 +1,30 @@
 module.exports = {
-  /*
-  ** Headers of the page
-  */
   head: {
-    title: 'awesome-cheatsheets',
+    title: "Awesome Cheatsheets",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Awesome cheatsheets for popular programming languages, frameworks and development tools. They include everything you should know in one single file.' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Awesome cheatsheets for popular programming languages, frameworks and development tools. They include everything you should know in one single file."
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
-  /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
+  loading: { color: "#3B8070" },
   build: {
-    /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     }
-  }
-}
-
+  },
+  css: ["normalize.css/normalize.css"]
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,21 @@
-<template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        awesome-cheatsheets
-      </h1>
-      <h2 class="subtitle">
-        Awesome cheatsheets for popular programming languages, frameworks and development tools. They include everything you should know in one single file.
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+<!-- *************************************************************************
+     TEMPLATE
+     ************************************************************************* -->
+
+<template lang="pug">
+
 </template>
 
-<script>
-import AppLogo from '~/components/AppLogo.vue'
+<!-- *************************************************************************
+     SCRIPT
+     ************************************************************************* -->
 
-export default {
-  components: {
-    AppLogo
-  }
-}
+<script>
+export default {};
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<!-- *************************************************************************
+     STYLE
+     ************************************************************************* -->
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
-
+<style lang="scss"></style>
