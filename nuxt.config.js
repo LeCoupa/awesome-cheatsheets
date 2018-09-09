9db045a6cb09238a2709111db97a98dc676505c7
Julien <julien.le.coupanec@gmail.com>
2018-09-09 17:07:37 +0200

    build(packages): add autoprefixer

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,8 @@ module.exports = {
           exclude: /(node_modules)/
         });
       }
-    }
+    },
+    postcss: [require("autoprefixer")()]
   },
   css: ["normalize.css/normalize.css"]
 };
