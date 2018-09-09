2c5a47182a39ec0460a02c705ea441e6b4e3d954
Julien <julien.le.coupanec@gmail.com>
2018-09-10 00:34:30 +0200

    feat(ga): only send events in production

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,8 @@ module.exports = {
     [
       "@nuxtjs/google-analytics",
       {
-        id: "UA-125493236-1"
+        id: "UA-125493236-1",
+        debug: { sendHitTask: process.env.NODE_ENV === "production" }
       }
     ]
   ],
