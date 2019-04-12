5f761745ab90818b28f9e382e1007f860d2c6890
HemendraKhatik <37813372+HemendraKhatik@users.noreply.github.com>
2019-04-13 05:02:27 +0530

    Flask framework cheat sheet.
    A basic cheat sheet about how to install flask and create a project hierarchy and run a flask application.

---
+++ backend/flask.py
@@ -0,0 +1,35 @@
+# *****************************************************************************
+# FLASK cheat sheet created by Hemendra Khatik.
+# *****************************************************************************
+
+
+# Installation
+# *****************************************************************************
+# pip install Flask
+# *****************************************************************************
+
+
+# Basic project layout
+# *****************************************************************************
+# application.py
+# static/  
+#     images
+#      /abc.jpg
+#     css
+#      /style.css
+#     js
+#      /xyz.js
+# templates/
+#    blog_post.html
+#    index.html
+# *****************************************************************************
+
+
+# Run FLASK application in git-bash terminal
+# *****************************************************************************
+# 1. Set environment variables
+#  a. export FLASK_APP=application.py -> This can be anything in my case according to line 14. It is application.py
+#  b. export DATABASE_URL="DATABASE URL" -> Set your database URL here
+# 2. Now run below command
+#   flask run
+# *****************************************************************************
