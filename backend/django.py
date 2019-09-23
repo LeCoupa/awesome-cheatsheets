a38e14178901632c313077c47f70734112c6e005
Manoj Tyagi <38884133+neo-0224@users.noreply.github.com>
2019-09-24 01:30:10 +0530

    Some more django-admin commands added
    I have added some more useful and some advanced django-admin commands with there use and description in a proper format.

--- backend/django.py
+++ backend/django.py
@@ -50,3 +50,28 @@ django-admin makemigrations              # create new migrations to the database
 django-admin runserver                   # start the development webserver at 127.0.0.1 with the port 8000
 django-admin help                        # display usage information and a list of the commands provided by each application
 django-admin version                     # display the current django version
+
+
+# ******************************************************************************
+# Other useful or advanced django-admin commands                                   
+# ******************************************************************************
+
+
+django-admin check                       # Checks the entire django project for potential problems
+django-admin compilemessages             # Compiles .po files to .mo files for use with builtin gettext support
+django-admin createcachetable            # Creates the tables needed to use the SQL cache backend.
+django-admin dbshell                     # Runs the command-line client for specified database, or the default database if none is provided.
+django-admin diffsettings                # Displays differences between the current settings.py and Django's default settings.
+django-admin dumpdata                    # Output the contents of the database as a fixture of the given format (using each model's default manager unless --all is specified).
+django-admin flush                       # Removes ALL DATA from the database, including data added during migrations. Does not achieve a "fresh install" state.
+django-admin inspectdb                   # Introspects the database tables in the given database and outputs a Django model module.
+django-admin loaddata                    # Installs the named fixture(s) in the database.
+django-admin makemessages                # Runs over the entire source tree of the current directory and pulls out all strings marked for translation. It creates (or updates) a message file in the conf/locale (in the django tree) or locale (for projects and applications) directory. You must run this command with one of either the --locale, --exclude, or --all options.
+django-admin sendtestemail               # Sends a test email to the email addresses specified as arguments.
+django-admin shell                       # Runs a Python interactive interpreter. Tries to use IPython or bpython, if one of them is available. Any standard input is executed as code.
+django-admin showmigrations              # Shows all available migrations for the current project.
+django-admin sqlflush                    # Returns a list of the SQL statements required to return all tables in the database to the state they were in just after they were installed.
+django-admin sqlmigrate                  # Prints the SQL statements for the named migration.
+django-admin sqlsequencereset            # Prints the SQL statements for resetting sequences for the given app name(s).
+django-admin squashmigrations            # Squashes an existing set of migrations (from first until specified) into a single new one.
+django-admin testserver                  # Runs a development server with data from the given fixture(s).
