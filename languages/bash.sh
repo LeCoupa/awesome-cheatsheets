6fe0435c7777df8de98b5d9f3536820c6a39c431
Alexound <alexound.login@gmail.com>
2019-10-24 12:02:15 +0400

    Keep working on 'bash' cheetsheet.
    Minor corrections.

--- languages/bash.sh
+++ languages/bash.sh
@@ -54,7 +54,7 @@ echo $SHELL         # displays the shell you're using
 echo $BASH_VERSION  # displays bash version
 
 bash                # if you want to use bash (type exit to go back to your previously opened shell)
-whereis bash        # finds out where bash is on your system
+whereis bash        # locates the binary, source and manual-page for a command
 which bash          # finds out which program is executed as 'bash' (default: /bin/bash, can change across environments)
 
 clear               # clears content on window (hide displayed lines)
