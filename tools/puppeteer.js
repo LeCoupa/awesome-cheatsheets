b3444b6a31ab7d2f310d640cd99c27e451388a90
Julien Le Coupanec <julien.le.coupanec@gmail.com>
2020-03-16 15:26:48 +0100

    docs(puppeteer): initialize cheatsheet

---
+++ tools/puppeteer.js
@@ -0,0 +1,14 @@
+/* *******************************************************************************************
+ * PUPPETEER
+ * https://pptr.dev/
+ * ******************************************************************************************* */
+
+
+// When you install Puppeteer, it downloads a recent version of Chromium (~170MB Mac, ~282MB Linux, ~280MB Win)
+// that is guaranteed to work with the API. 
+npm install puppeteer;
+
+// A lightweight version of Puppeteer for launching an existing browser installation or for connecting to a remote one.
+// Be sure that the version of puppeteer-core you install is compatible with the browser you intend to connect to.
+npm install puppeteer-core; 
+
