0b29b9bb5419eaf51b3516dc8ac2e683b13bf189
Julien Le Coupanec <julien.le.coupanec@gmail.com>
2020-04-10 18:00:31 +0200

    docs(adonis): add migration commands

--- backend/adonis.js
+++ backend/adonis.js
@@ -8,28 +8,39 @@
  * AVAILABLE CLI COMMANDS
  ********************************************************************************************/
 
-build                 // Compile typescript code to Javascript. Optionally watch for file changes
-invoke                // Invoke post install instructions on a given AdonisJs package
-serve                 // Compile typescript code to Javascript and start the HTTP server
+build   // Compile typescript code to Javascript. Optionally watch for file changes
+invoke  // Invoke post install instructions on a given AdonisJs package
+serve   // Compile typescript code to Javascript and start the HTTP server
 
 // Dump
 
-dump:rcfile           // Dump contents of .adonisrc.json file along with defaults
+dump:rcfile  // Dump contents of .adonisrc.json file along with defaults
 
 // Generate
 
-generate:key          // Generate a new APP_KEY secret
-generate:manifest     // Generate manifest file to execute ace commands
+generate:key       // Generate a new APP_KEY secret
+generate:manifest  // Generate manifest file to execute ace commands
 
 // List
 
-list:routes           // List application routes
+list:routes  // List application routes
 
 // Make
 
-make:command          // Make a new ace command
-make:controller       // Make a new HTTP controller
-make:middleware       // Make a new middleware
-make:provider         // Make a new IoC container provider
-make:validator        // Make a new validator
-make:view             // Make a new view template
+make:command     // Make a new ace command
+make:controller  // Make a new HTTP controller
+make:middleware  // Make a new middleware
+make:migration   // Make a new migration
+make:provider    // Make a new IoC container provider
+make:validator   // Make a new validator
+make:view        // Make a new view template
+
+// Migrations
+
+make:migration      // Create a new migration file.
+migration:run       // Run all pending migrations.
+migration:rollback  // Rollback last set of migrations.
+migration:refresh   // Rollback all migrations to the 0 batch then re-run them from the start.
+migration:reset     // Rollback all migrations to the 0 batch.
+migration:status    // Get the status of all the migrations.
+
