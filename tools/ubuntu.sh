328b62fcb892e37e599c161f7f4772529197c14b
Julien Le Coupanec <julien.le.coupanec@gmail.com>
2020-06-21 18:38:14 +0200

    docs(ubuntu): df -h command

--- tools/ubuntu.sh
+++ tools/ubuntu.sh
@@ -3,3 +3,5 @@
 ##############################################################################
 
 scp /path/to/file user@server:/path/to/destination # Copy file from local to server
+
+df -h # Check the amount of free space
