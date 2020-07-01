d2e6e17e9eb700c803408e45fad738f45e1c413c
Julien Le Coupanec <julien.le.coupanec@gmail.com>
2020-07-01 11:31:00 +0200

    docs(ubuntu): add commands

--- tools/nginx.sh
+++ tools/nginx.sh
@@ -5,4 +5,5 @@
 
 sudo nginx -t # Check syntax
 sudo systemctl reload nginx # Reload nginx
+sudo service nginx restart  # Restart nginx
 sudo ln -s /etc/nginx/sites-available/example.com /etc/nginx/sites-enabled/ # Link website
--- tools/ubuntu.sh
+++ tools/ubuntu.sh
@@ -5,3 +5,6 @@
 scp /path/to/file user@server:/path/to/destination # Copy file from local to server
 
 df -h # Check the amount of free space
+
+sudo ufw status # Check status
+sudo ufw allow from remote_IP_address to any port 3306 # Allow external ip to access port
