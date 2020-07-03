4d9adef433c452ba828416625040a6dddf66d8b7
Julien Le Coupanec <julien.le.coupanec@gmail.com>
2020-07-03 14:29:06 +0200

    docs(mysql): SHOW PROCESSLIST

--- databases/mysql.sh
+++ databases/mysql.sh
@@ -4,3 +4,5 @@
 
 mysqldump -h hostname -u username -p database_name -P port > file.sql # Export database
 mysql -u username -p database_name < file.sql                         # Import database
+
+SHOW PROCESSLIST; # Show you any queries that are currently running or in the queue to run
