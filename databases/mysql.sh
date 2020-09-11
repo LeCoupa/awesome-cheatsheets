e8be8df3d65ff61ac5942e7abbdd375290ca8f6c
Julien Le Coupanec <julien.le.coupanec@gmail.com>
2020-09-11 11:02:31 +0200

    docs(mysql): grant all privileges

--- databases/mysql.sh
+++ databases/mysql.sh
@@ -6,3 +6,5 @@ mysqldump -h hostname -u username -p database_name -P port > file.sql # Export d
 mysql -u username -p database_name < file.sql                         # Import database
 
 SHOW PROCESSLIST; # Show you any queries that are currently running or in the queue to run
+
+GRANT ALL PRIVILEGES ON mydb.* TO 'myuser'@'%' WITH GRANT OPTION; # Grant **all** privileges on database
