731020d2e401faa32d7812585c85a355bfc6711a
Julien Le Coupanec <julien.le.coupanec@gmail.com>
2020-12-07 17:22:08 +0100

    docs(ubuntu): display ip of the server

--- tools/ubuntu.sh
+++ tools/ubuntu.sh
@@ -8,6 +8,7 @@ df -h # Check the amount of free space
 
 sudo ufw status # Check status
 sudo ufw allow from remote_IP_address to any port 3306 # Allow external ip to access port
+sudo ufw allow from 68.183.116.178 to any port 3306 # Allow external ip to access port
 
 scp user@remote_host:remote_file local_file # download: remote -> local
 scp local_file user@remote_host:remote_file # upload: local -> remote
@@ -15,3 +16,5 @@ scp local_file user@remote_host:remote_file # upload: local -> remote
 sudo -s # Log as root
 
 cat /proc/<process_id>/maps   # Show the current virtual memory usage of a Linux process
+
+ip r # Display ip of the server
