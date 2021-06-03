81add6e07b15939bafb6e6ff585b682aa2dd1669
Zoheb Alli Khan <understandingzak@gmail.com>
2021-06-03 09:54:44 +0530

    Added all express methods

--- backend/express.js
+++ backend/express.js
@@ -49,3 +49,51 @@ mergeParams               //if param names of child and parent are conflicted th
 
 strict                    // Enables Strict routing
 `;
+
+express.static(root, [options]);
+
+options: `
+dotfiles                 // determines how dotfiles are used
+
+etag                     // Operates etag generation
+
+extensions               // Operates file extension fallback
+
+fallthrough              //	Enable or disable the immutable directive in the Cache-Control response header
+
+index                    // sends the specified directory index file
+
+LastModified             // sets the Last-Modified header to the last modified date
+
+setHeaders               // Function for setting HTTP headers to serve with the file
+`;
+
+express.text([options]);
+
+options: `
+defaultCharset          // Sets the default charset for the text context.
+
+inflate                 // to manage the deflated bodies like enabling and disabling
+
+limit                   // Controls the maximum request body size.
+
+type                    // This is used to determine the type of middleware will parse
+
+verify                  // It is an undefined function which used to verify the middleware parsing.
+`;
+
+express.urlencoded([options]);
+
+options: `
+extended                // it allows to choose between parsing the URL-encoded data or the qs library
+
+parameterLimit          // It controls the no of params.
+
+inflate                 // to manage the deflated bodies like enabling and disabling
+
+limit                   // Controls the maximum request body size.
+
+type                    // This is used to determine the type of middleware will parse
+
+verify                  // It is an undefined function which used to verify the middleware parsing.
+`;
