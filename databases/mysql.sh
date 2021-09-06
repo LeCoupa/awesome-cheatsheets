b443850b2a42e42aea9ac127c5305a8b3cb02c53
Julien Le Coupanec <julien.le.coupanec@gmail.com>
2021-09-06 12:22:38 +0200

    docs: mysql update

--- databases/mysql.sh
+++ databases/mysql.sh
@@ -7,8 +7,13 @@ mysql -u username -p database_name < file.sql                         # Import d
 
 SHOW PROCESSLIST; # Show you any queries that are currently running or in the queue to run
 
+show status where `variable_name` = 'Threads_connected'; # Show all connected threads
+show variables like 'max_connections'; # Show maximum number of allowed connections
+SET GLOBAL max_connections = 150; ## Set new value for maximum connections (no restart needed)
+
 GRANT ALL PRIVILEGES ON prospectwith.* TO 'power'@'localhost' WITH GRANT OPTION; # Grant all privileges on database
 
 CREATE USER 'newuser'@'localhost' IDENTIFIED BY 'password'; # Create user
 
 mysql -u root -pmypassword -e "MY SQL QUERY" &>> query.log & disown # Run SQL query in the background
+
