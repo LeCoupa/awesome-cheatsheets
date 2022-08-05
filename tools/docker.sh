b095a3534df561a36b5b7c5356a3b92d19bd4d32
HemanthKumar M <m.hemanthkmr143@gmail.com>
2022-08-05 15:31:25 +0530

    added rm -f in docker.sh
    Kindly Verify it

--- tools/docker.sh
+++ tools/docker.sh
@@ -12,6 +12,7 @@ docker stop <hash>                          # Gracefully stop the specified cont
 docker ps -a                                # See a list of all containers, even the ones not running
 docker kill <hash>                          # Force shutdown of the specified container
 docker rm <hash>                            # Remove the specified container from this machine
+docker rm -f <hash>                         # Remove force specified container from this machine
 docker rm $(docker ps -a -q)                # Remove all containers from this machine
 docker images -a                            # Show all images on this machine
 docker rmi <imagename>                      # Remove the specified image from this machine
