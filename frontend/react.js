58f626bb22e5ad59fc708aefcff3f2fb0787b2b9
Caramujo-San <gustavocjorge11@yahoo.com.br>
2023-02-15 17:12:04 -0300

    Update react.js

--- frontend/react.js
+++ frontend/react.js
@@ -136,7 +136,8 @@ class Component extends React.Component {
 
   // Invoked immediately after updating occurs. This method is not called for the initial render.
   // Use this as an opportunity to operate on the DOM when the component has been updated.
-  // This is also a good place to do network requests as long as you compare the current props to previous props (e.g. a network request may not be necessary if the props have not changed).
+  // This is also a good place to do network requests as long as you compare the current props to  
+  // previous props (e.g. a network request may not be necessary if the props have not changed).
   componentDidUpdate(prevProps, prevState) { }
 
   // Invoked immediately before a component is unmounted and destroyed.
